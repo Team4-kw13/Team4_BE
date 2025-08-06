c310197fd4cc42498bedd1104f99afafac93a866
kymin <park48664866@gmail.com>
2025-08-06 14:48:22 +0900

    refactor: 와일드 카드 제거 및 메서드와 헤더 명시

--- src/main/java/org/team4/hanzip/global/config/CorsConfig.java
+++ src/main/java/org/team4/hanzip/global/config/CorsConfig.java
@@ -2,6 +2,8 @@ package org.team4.hanzip.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -14,10 +16,21 @@ public class CorsConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
 		corsConfiguration.setAllowCredentials(true);
+
 		corsConfiguration.addAllowedOrigin("http://localhost:3000");
-		corsConfiguration.addAllowedMethod("*");
-		corsConfiguration.addAllowedHeader("*");
-		corsConfiguration.addExposedHeader("Authorization");
+
+		corsConfiguration.addAllowedMethod(HttpMethod.GET);
+		corsConfiguration.addAllowedMethod(HttpMethod.POST);
+		corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+		corsConfiguration.addAllowedMethod(HttpMethod.PATCH);
+		corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+		corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS);
+
+		corsConfiguration.addAllowedHeader(HttpHeaders.AUTHORIZATION);
+		corsConfiguration.addAllowedHeader(HttpHeaders.CONTENT_TYPE);
+		corsConfiguration.addAllowedHeader(HttpHeaders.ACCEPT);
+
+		corsConfiguration.addExposedHeader(HttpHeaders.AUTHORIZATION);
 
 		source.registerCorsConfiguration("/**", corsConfiguration);
 
