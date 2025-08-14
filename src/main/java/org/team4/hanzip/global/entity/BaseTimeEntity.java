06e0f8f26fff5c15729ca134facb73ea2b9c5b5c
JJhwan7099 <ghks7099@naver.com>
2025-08-14 19:39:46 +0900

    refactor: BaseTimeEntity 추상 클래스로 변경 및 Getter 어노테이션 추가

--- src/main/java/org/team4/hanzip/global/entity/BaseTimeEntity.java
+++ src/main/java/org/team4/hanzip/global/entity/BaseTimeEntity.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,7 +13,8 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseTimeEntity {
+@Getter
+public abstract class BaseTimeEntity {
     @CreatedDate
     @Column(updatable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
