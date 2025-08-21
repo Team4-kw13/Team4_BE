78b691533a32acf76d141372cdbd9c8f17793e06
kymin <park48664866@gmail.com>
2025-08-22 00:08:28 +0900

    feat: 계약서 이미지 업로드 관련 상태코드 추가

--- src/main/java/org/team4/hanzip/global/api/code/contract/ErrorCode.java
+++ src/main/java/org/team4/hanzip/global/api/code/contract/ErrorCode.java
@@ -9,7 +9,12 @@ import org.team4.hanzip.global.api.code.ErrorResultCode;
 @RequiredArgsConstructor
 public enum ErrorCode implements ErrorResultCode {
 	INVALID_ARGUMENT(HttpStatus.BAD_REQUEST, "올바르지 않은 요청값입니다."),
-	CONTRACT_NOT_FOUND(HttpStatus.NOT_FOUND, "데이터가 존재하지 않습니다.");
+	CONTRACT_NOT_FOUND(HttpStatus.NOT_FOUND, "데이터가 존재하지 않습니다."),
+
+	// 이미지 업로드 시 예외
+	FILE_NOT_FOUND(HttpStatus.BAD_REQUEST, "파일이 존재하지 않습니다."),
+	FILE_EXTENSION_NOT_FOUND(HttpStatus.BAD_REQUEST, "파일 확장자가 존재하지 않습니다."),
+	INVALID_FILE_EXTENSION(HttpStatus.BAD_REQUEST, "올바르지 않은 파일 확장자입니다.");
 
 	private final HttpStatus status;
 	private final String message;
